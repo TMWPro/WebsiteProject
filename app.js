9fe2bf90e8bc66f6315a91a4a89301ab49fb2527
TMWPro <92699981+TMWPro@users.noreply.github.com>
2022-05-15 19:13:41 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Creating a variable that interacts with DOM
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+// Mobile Menu Display
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
